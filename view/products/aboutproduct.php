4fcf8e7e43d9d4422922bc71c012f895d0f8d548
phinv2002 <phinvph29001@fpt.edu.vn>
2023-03-13 08:43:00 +0700

    sanphamchitiet

--- view/products/aboutproduct.php
+++ view/products/aboutproduct.php
@@ -1,27 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<title>ok</title>
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
-    <title>Shop Item - Start Bootstrap Template</title>
-    <!-- Favicon-->
-    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
-    <!-- Bootstrap icons-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
-    <!-- Core theme CSS (includes Bootstrap)-->
-    <link href="css/styles.css" rel="stylesheet" />
-    
-    
-</head>
-
-<body>
-    <!-- Navigation-->
-
-    <!-- Product section-->
-    <section class="py-5">
+<section class="py-5">
         <div class="container px-4 px-lg-5 my-5">
             
             <?php
@@ -181,16 +158,3 @@
             </div>
         </div>
     </section>
-    <!-- Footer-->
-    <footer class="py-5 bg-dark">
-        <div class="container">
-            <p class="m-0 text-center text-white">Copyright &copy; Your Website 2022</p>
-        </div>
-    </footer>
-    <!-- Bootstrap core JS-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-    <!-- Core theme JS-->
-    <script src="js/scripts.js"></script>
-</body>
-
-</html>
