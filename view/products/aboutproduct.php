1ce548e85c8ae2692d56f8cf32c95f7194f58f4a
phinv2002 <phinvph29001@fpt.edu.vn>
2023-03-14 10:04:42 +0700

    giaodiensanphamchitiet

--- view/products/aboutproduct.php
+++ view/products/aboutproduct.php
@@ -27,7 +27,7 @@
     <!-- Related items section-->
     <section class="py-5 bg-light">
         <div class="container px-4 px-lg-5 mt-5">
-            <h2 class="fw-bolder mb-4">Related products</h2>
+            <h2 class="fw-bolder mb-4">Danh sách sản phẩm</h2>
             <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
                 <?php
                 
