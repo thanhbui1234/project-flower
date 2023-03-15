e4a4b78276b5b07485637eed921ad4c734ec6ec4
thanh <miktenthanh@gmail.com>
2023-03-15 23:01:52 +0700

    form categories

--- admin/view/categories/categories.php
+++ admin/view/categories/categories.php
@@ -1,13 +1,18 @@
+<!-- click hủy xóa form update -->
+<?php if (isset($_POST['cancelUpdate'])) {
+    header('location: /project-flower/admin/index.php?act=categories');
+}?>
+
 <div class="row mx-5 ">
 
     <div class="col">
-        <form method="post" class="" action="#">
+        <form method="post" id="form-addCategories" class="" action="#">
 
             <div class="form-group">
                 <label for="exampleInputEmail1"></label>
-                <input type="text" class="form-control" name="category" aria-describedby="emailHelp" placeholder="">
+                <input type="text" class="form-control" name="category" placeholder="">
 
-                <p> <?php echo isset($errCategory['category']) ? $errCategory['category'] : ''; ?></p>
+
             </div>
 
             <input class="btn btn-primary" value="Thêm Loại" name="addCategory" type="submit">
@@ -43,7 +48,7 @@
     </div>
 
 
-
+    <?php showCategories()?>
     <div class="col">
         <table class="table table-bordered  mt-3">
             <thead>
@@ -58,11 +63,11 @@
             <tbody>
 
 
-
+                <?php foreach ($dataCategories as $categories) {extract($categories)?>
 
                 <tr>
-                    <td> 1</td>
-                    <td>hoa</td>
+                    <td> <?php echo $id ?></td>
+                    <td><?php echo $name ?></td>
                     <td><a class="btn btn-success" href="index.php?act=categories&&update=1">UPDATE</a>
                         <a class="delete_categories" data-id="<?php echo $id ?>"><button
                                 class="btn btn-danger">DELETE</button></a>
@@ -70,18 +75,11 @@
 
                 </tr>
 
+                <?php }?>
             </tbody>
 
         </table>
     </div>
-
 </div>
 
-
-
-
-
-</div>
-
-
-</div>
+<script src="layout/js/categories.js"></script>
