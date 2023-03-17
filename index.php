e1e63d1288360f45fbfb438ea02cfd25c93fbe63
thanh <miktenthanh@gmail.com>
2023-03-17 23:22:20 +0700

    connect db

--- index.php
+++ index.php
@@ -1,58 +1,53 @@
-
 <?php include './layout/header.php';
 include './model/product.php';
 include './model/category.php';
 
 ?>
+
 <body id="page-top">
     <?php
-    load_name_category();
-    include './layout/nav.php';
-    ?>
+load_name_category();
+include './layout/nav.php';
+?>
 
     <?php
 
-    showproduct();
-    ?>
+showproduct();
+?>
     <?php isset($_GET['act']) ? $url = $_GET['act'] : $url = false;
 
-    switch ($url) {
+switch ($url) {
+
+    case 'aboutproducts';
+        showaboutproducts();
+        showproduct();
+        include './view/products/aboutproduct.php';
+        break;
+
+    case 'category';
+        loadcategory();
+        include './view/category/category.php';
+
+        break;
+    case 'search';
+        search();
+        include './view/search/search.php';
+        break;
+
+    case 'profile';
+        include './view/profile/profile.php';
+        break;
+
+    case 'changepassword';
+        include './view/profile/changepassword.php';
+        break;
+    default:
+        include './view/products/product.php';
+        break;
+}
+
+?>
 
 
 
-        case 'aboutproducts';
-            showaboutproducts();
-            showproduct();
-            include './view/products/aboutproduct.php';
-            break;
-
-
-        case 'category';
-            loadcategory();
-            include './view/category/category.php';
-
-            break;
-        case 'search';
-            search();
-            include './view/search/search.php';
-            break;
-
-        case 'profile';
-            include './view/profile/profile.php';
-            break;
-
-        case 'changepassword';
-            include './view/profile/changepassword.php';
-            break;
-        default:
-            include './view/products/product.php';
-            break;
-    }
-
-    ?>
-
-
-
-    <?php include './layout/footer.php' ?>
-
-
+    <?php include './layout/footer.php'?>
