26b4e75218311228caace1224615933c2be68f32
thanh <miktenthanh@gmail.com>
2023-03-17 11:29:08 +0700

    categories home

--- index.php
+++ index.php
@@ -4,27 +4,20 @@
    <body id="page-top">
        <?php include './layout/nav.php'?>
 
-
+       <?php require_once './model/search.php'?>
 
 
        <?php isset($_GET['act']) ? $url = $_GET['act'] : $url = false;
 
 switch ($url) {
 
-    case 'he';
-        echo "hi";
-        break;
     case 'aboutproduct';
         include './view/products/aboutproduct.php';
         break;
     case 'search';
-
+        search();
         include './view/search/search.php';
         break;
-    // case 'logout';
-    //     include './view/logout/logout.php';
-
-    //     break;
 
     case 'profile';
         include './view/profile/profile.php';
