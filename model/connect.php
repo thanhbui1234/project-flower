e71fdc10298b9e48f2e603efbf6582f7429c30ca
phinv2002 <phinvph29001@fpt.edu.vn>
2023-03-20 19:27:18 +0700

    edit-connect

--- model/connect.php
+++ model/connect.php
@@ -4,7 +4,7 @@ $username = "root";
 $password = "";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=duan1", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=project-flower", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     // echo " connect db thnanh cong ";
