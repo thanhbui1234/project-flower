dfb42a6217ad4bbd3b1a8559787dd7739ef790a6
thanh <miktenthanh@gmail.com>
2023-03-23 10:25:25 +0700

    ko up

--- admin/index.php
+++ admin/index.php
@@ -8,7 +8,7 @@
 <?php require_once './models/product.php'?>
 <?php require_once './models/categories.php'?>
 
-<?php include './models/categories.php' ?>
+<?php include './models/categories.php'?>
 
 
 
@@ -39,7 +39,7 @@ switch ($act) {
         break;
 
     default:
-        
+
         include './view/home/home.php';
         break;
 }
