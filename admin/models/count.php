bc6cd4521e6cd7eb52ac52904e9fe77dfefeab4e
Nguyen Van Ha <hanvph29030@fpt.edu.vn>
2023-04-05 01:56:31 +0700

    update

--- admin/models/count.php
+++ admin/models/count.php
@@ -24,15 +24,68 @@ function countAll()
     global $countUsers;
     $countUsers = $statement->fetchColumn();
 
+    $sqlSaleProd = "SELECT COUNT(*) FROM products WHERE deal > 0";
+    $statement = $conn->prepare($sqlSaleProd);
+    $statement->execute();
+    global $dataSaleProd;
+    $dataSaleProd = $statement->fetchColumn();
+
     $sqlComment = "SELECT COUNT(*) FROM comments";
     $statement = $conn->prepare($sqlComment);
     $statement->execute();
     global $countComments;
     $countComments = $statement->fetchColumn();
 
-    $sqlViewMax = "SELECT max(view) as maxView, name FROM products";
+    $sqlCmtChuaduyet = "SELECT COUNT(*) FROM comments WHERE trangthai = 'chưa duyệt'";
+    $statement = $conn->prepare($sqlCmtChuaduyet);
+    $statement->execute();
+    global $CmtChuaduyet;
+    $CmtChuaduyet = $statement->fetchColumn();
+
+    $sqlProdunconfimred = "SELECT COUNT(*) FROM bills WHERE status = 'No_confirm'";
+    $statement = $conn->prepare($sqlProdunconfimred);
+    $statement->execute();
+    global $Produnconfimred;
+    $Produnconfimred = $statement->fetchColumn();
+
+    $sqlProdconfirmed = "SELECT COUNT(*) FROM bills WHERE status = 'delivering'";
+    $statement = $conn->prepare($sqlProdconfirmed);
+    $statement->execute();
+    global $Prodconfirmed;
+    $Prodconfirmed = $statement->fetchColumn();
+
+    //Sản phẩm có lượt xem nhiều nhất
+    $sqlViewMax = "SELECT name, max(view) FROM products";
     $statement = $conn->prepare($sqlViewMax);
     $statement->execute();
-    global $dataViewMax;
-    $dataViewMax = $statement->fetchAll();
+    $ViewMax = $statement->fetchAll();
+    foreach($ViewMax as $ViewMax1) {
+        global $dataViewMax;
+        $dataViewMax = $ViewMax1['max(view)'];
+    }
+    
+    $sqlSelectNameViewMax = "SELECT name FROM products WHERE view = '$dataViewMax'";
+    $statement = $conn ->prepare($sqlSelectNameViewMax);
+    $statement ->execute();
+    global $dataNameViewMax;
+    $dataNameViewMax = $statement ->fetchAll();
+
+    
+    //Sản phẩm có lượt mua nhiều nhất
+    $sqlbuyMax = "SELECT max(amount) FROM bill_detail INNER JOIN bills ON bills.id = bill_detail.bill WHERE status = 'delivering' ";
+    $statement = $conn->prepare($sqlbuyMax);
+    $statement->execute();
+    $buyMax = $statement->fetchAll();
+    foreach ($buyMax as $buyMax1) {
+        global $dataBuymax;
+        $dataBuymax = $buyMax1['max(amount)'];
+    }
+
+    $sqlSelectNameBuyMax = " SELECT productName FROM bill_detail WHERE amount = $dataBuymax ";
+    $statement = $conn->prepare($sqlSelectNameBuyMax);
+    $statement->execute();
+    global $dataNameBuyMax;
+    $dataNameBuyMax = $statement->fetchAll();
+
+    
 }
--- admin/view/home/home.php
+++ admin/view/home/home.php
@@ -76,17 +76,103 @@
             </div>
         </div>
 
-        <!-- Pending Requests Card Example -->
         <div class="col-xl-3 col-md-6 mb-4">
             <div class="card border-left-warning shadow h-100 py-2">
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                Bình Luận
+                                Sản phẩm đang sale
+                            </div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                <?php echo $dataSaleProd ?>
+                            </div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fa-solid fa-comment  fa-2x text-gray-300 "></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Pending Requests Card Example -->
+
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                Bình luận
                             </div>
                             <div class="h5 mb-0 font-weight-bold text-gray-800">
                                 <?php echo $countComments ?>
+                            </div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fa-solid fa-comment  fa-2x text-gray-300 "></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-success shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                Bình luận chưa duyệt
+                            </div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                <?php echo $CmtChuaduyet ?>
+                            </div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fa-solid fa-table fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-info shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
+                                Đơn hàng chưa được xác nhận
+                            </div>
+                            <div class="row no-gutters align-items-center">
+                                <div class="col-auto">
+                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">
+                                        <?php echo $Produnconfimred ?>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fa-solid fa-user fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-warning shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                                Đơn hàng đã được xác nhận
+                            </div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                <?php echo $Prodconfirmed ?>
                             </div>
                         </div>
                         <div class="col-auto">
@@ -103,14 +189,13 @@
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
                             <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                Sản phẩm có nhiều lượt xem nhất
+                                Sản phẩm mua nhiều nhất
                             </div>
                             <div class="h5 mb-0 font-weight-bold text-gray-800">
-                                <?php 
-                                    foreach ($dataViewMax as $viewMax) {
-                                        // print_r($viewMax);
-                                        echo $viewMax['name'];
-                                    }
+                                <?php
+                                    foreach ($dataNameBuyMax as $name) {
+                                        echo $name['productName'];
+                                    };
                                 ?>
                             </div>
                         </div>
@@ -122,12 +207,36 @@
             </div>
         </div>
 
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-success shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                Sản phẩm được xem nhiều nhất
+                            </div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                <?php
+                                    foreach ($dataNameViewMax as $name) {
+                                        echo $name['name'];
+                                    };
+                                ?>
+                            </div>
+                        </div>
+                        <div class="col-auto">
+                            <i class="fa-solid fa-table fa-2x text-gray-300"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 
 
 
     <!-- GOOGLE CHART -->
-    <h2 class="text-center">Biểu đồ</h2>
+    <h2 class="text-center text-uppercase mt-3">Biểu đồ</h2>
     <div id="chart">
         <canvas id="myChart"></canvas>
     </div>
@@ -138,41 +247,48 @@
 
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script>
-        const ctx = document.getElementById('myChart');
+    const ctx = document.getElementById('myChart');
 
-        new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: ['Sản phẩm', 'Loại sản phẩm', 'Người dùng', 'Bình luận'],
-                datasets: [{
-                    label: 'Đơn vị',
-                    data: [
-                        <?php echo $countProducts ?>,
-                        <?php echo $countCategories ?>,
-                        <?php echo $countUsers ?>,
-                        <?php echo $countComments ?>,
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Sản phẩm', 'Loại sản phẩm', 'Người dùng', 'Sản phẩm đang sale', 'Bình luận',
+                'Bình luận chưa duyệt', 'Đơn hàng chua được xác nhận', 'Đơn hàng đã được xác nhận'
+            ],
+            datasets: [{
+                label: 'Đơn vị',
+                data: [
+                    <?php echo $countProducts ?>,
+                    <?php echo $countCategories ?>,
+                    <?php echo $countUsers ?>,
+                    <?php echo $countComments ?>,
+                    <?php echo $countComments ?>,
+                    <?php echo $CmtChuaduyet ?>,
+                    <?php echo $Produnconfimred ?>,
+                    <?php echo $Prodconfirmed ?>
 
-                    ],
-                    borderWidth: 2
-                }]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
+
+                ],
+                borderWidth: 2
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
                 }
             }
-        });
+        }
+    });
     </script>
     <!-- END GOOGLE CHART -->
 
     <style>
-        #chart {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-        }
+    #chart {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
     </style>
 
 
