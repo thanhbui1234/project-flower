7d6be386ce56e8c1e856635b449709abaa958b7a
Beo kutee <hunglcvph28989@fpt.edu.vn>
2023-04-11 10:29:09 +0700

    fixed

--- admin/view/home/home.php
+++ admin/view/home/home.php
@@ -57,7 +57,7 @@
 <div class="container-fluid">
     <div class="tabs">
         <div class="tab-item active">Thống kê</div>
-        <div class="tab-item">Doanh thu</div>
+        <!-- <div class="tab-item">Doanh thu</div> -->
         <div class="line"></div>
     </div>
 
